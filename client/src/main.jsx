9d23e06ed609ac2ce7403519f8c10225f9386694
Pratik <pratikh004@gmail.com>
2025-09-01 08:15:11 +0530

    minor fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import { AppContextProvider } from './context/AppContext.jsx'
 import { TeamContextProvider } from './context/TeamContext.jsx'
+import axios from 'axios'
+
+axios.defaults.withCredentials = true
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
